987c51a3e33da5c7626a0f8b8a83d243109f2412
Wouter Theune <theune32@gmail.com>
2019-05-25 23:27:42 +0200

    compiling version of drive_bot

---
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -0,0 +1,56 @@
+#include "ros/ros.h"
+#include "ball_chaser/DriveToTarget.h"
+#include "geometry_msgs/Twist.h"
+#include <std_msgs/Float64.h>
+
+class BallChaser
+{
+public:
+  BallChaser()
+  {
+    // Define a publisher for publishing commands for the robots actuator
+    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    // Define a service for handling drive requests
+    command_robot = n.advertiseService("/ball_chaser/command_robot", &BallChaser::handle_drive_request, this);
+    ROS_INFO("Ready to drive requests");
+
+  }
+
+// Handles the drive request and publishes them towards the actuator
+bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
+    ball_chaser::DriveToTarget::Response& res)
+{
+
+    ROS_INFO("DriveToTarget received - lin_x:%1.2f, ang_z:%1.2f", (float)req.linear_x, (float)req.angular_z);
+    geometry_msgs::Twist msg;
+    msg.linear.x = req.linear_x;    msg.angular.z = req.angular_z;
+
+    // Publishes wheel velocties for the actuator
+    motor_command_publisher.publish(msg);
+
+    // Return a response message
+    res.msg_feedback = "Requested wheel velocities set - lin_x:%1.2f, ang_z:%1.2f", (float)req.linear_x, (float)req.angular_z;
+    ROS_INFO_STREAM(res.msg_feedback);
+
+    return true;
+}
+
+private:
+  ros::NodeHandle n; 
+  ros::Publisher motor_command_publisher;
+  ros::ServiceServer command_robot;
+};
+
+int main(int argc, char **argv)
+{
+  //Initiate ROS
+  ros::init(argc, argv, "ball_chaser");
+
+  //Create Ballchaser object that handles everything
+  BallChaser ballChaser;
+
+  ros::spin();
+
+  return 0;
+}
